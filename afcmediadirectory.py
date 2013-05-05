a4978256ed29ad012033bb425acd70704a35b42d
Mountainstorm <noreply@mountainstorm.co.uk>
2013-05-05 11:19:21 +0100

    Added some initial command line support for afc - more to do

--- afcmediadirectory.py
+++ afcmediadirectory.py
@@ -33,7 +33,7 @@ class AFCMediaDirectory(AFC):
 			raise RuntimeError(u'Unable to launch:', AMSVC_AFC)
 		AFC.__init__(self, s)
 
-def transfer_application(self, path, progress=None):
+	def transfer_application(self, path, progress=None):
 		u'''Transfers an application bundle to the device; into the 
 		/PublicStaging directory
 
@@ -61,32 +61,119 @@ def transfer_application(self, path, progress=None):
 			raise RuntimeError(u'Unable to transfer application')
 
 
-if __name__ == u'__main__':
+def register_argparse_afc(cmdargs):
+	import argparse
 	import sys
+	import afcapplicationdirectory
+	import afccrashlogdirectory
+	import afcmediadirectory
+	import afcroot
+	import time
 
-	def printdir(afc, path):
+	def printdir(afc, path, recurse):
+		dirlist = []
 		for name in afc.listdir(path):
 			isdir = u''
-			if afc.lstat(path + name).st_ifmt == stat.S_IFDIR:
+			info = afc.lstat(path + name)
+			if info.st_ifmt == stat.S_IFDIR:
 				isdir = u'/'
-			print path + name + isdir
-			if afc.lstat(path + name).st_ifmt == stat.S_IFDIR:
-				printdir(afc, path + name + isdir)
-	
-	def factory(dev):
-		d = AMDevice(dev)
-		d.connect()
-		afc = AFCMediaDirectory(d)
-		printdir(afc, u'/') # recursive print of all files visible
+				dirlist.append(path + name + isdir)
 
-		AMDSetLogLevel(0xff)
-		afc.transfer_application(u'/Users/cooper/Documents/Dev/iOS/DeveloperDiskImg/Applications/MobileReplayer.app')
-		AMDSetLogLevel(0x0)
+			types = {
+				stat.S_IFSOCK: u's',
+				stat.S_IFLNK: u'l',
+				stat.S_IFREG: u'-',
+				stat.S_IFBLK: u'b',
+				stat.S_IFDIR: u'd',
+				stat.S_IFCHR: u'c',
+				stat.S_IFIFO: u'p'
+			}
+			t = time.asctime(time.gmtime(float(info.st_mtime)))
+			print(types[info.st_ifmt] + u' ' + t + u' ' + info.st_size + u' ' + name + isdir)
+		if recurse:
+			for name in dirlist:
+				print(u'\n' + name)
+				printdir(afc, name, recurse)
 
-		printdir(afc, u'/') # recursive print of all files visible
+	def get_afc(args, dev):
+		retval = None
+		if args.m:
+			retval = afcmediadirectory.AFCMediaDirectory(dev)
+		elif args.c:
+			retval = afccrashlogdirectory.AFCCrashLogDirectory(dev)
+		elif args.app is not None:
+			retval = afcapplicationdirectory.AFCApplicationDirectory(
+				dev, 
+				args.app.decode(u'utf-8')
+			)
+		else:
+			retval = afcroot.AFCRoot(dev)
+		return retval
 
+	def cmd_ls(args, dev):
+		afc = get_afc(args, dev)
+		printdir(afc, args.path.decode(u'utf-8'), args.r)
 		afc.disconnect()
-		return d
-	
-	handle_devices(factory)
+
+	# afc command
+	afcparser = cmdargs.add_parser(
+		u'afc', 
+		help=u'commands to manipulate files via afc'
+	)
+	afcgroup = afcparser.add_mutually_exclusive_group()
+	afcgroup.add_argument(
+		u'-a',
+		metavar=u'app',
+		dest=u'app',
+		help=u'reverse domain name of application; device paths become relative to app container'
+	)
+	afcgroup.add_argument(
+		u'-c',
+		action=u'store_true',
+		help=u'crashlogs; device paths become relative to crash log container'
+	)
+	afcgroup.add_argument(
+		u'-m',
+		action=u'store_true',
+		help=u'device paths become relative to /var/mobile/media (saves typing)'
+	)
+	afccmd = afcparser.add_subparsers()
+
+	# ls command
+	lscmd = afccmd.add_parser(
+		u'ls',
+		help=u'lists the contents of the directory'
+	)
+	lscmd.add_argument(
+		u'-r',
+		action=u'store_true',
+		help=u'if specified listing is recursive'
+	)
+	lscmd.add_argument(
+		u'path',
+		help=u'path on the device to list'
+	)
+	lscmd.set_defaults(func=cmd_ls)
+
+
+#if __name__ == u'__main__':
+#	import sys
+#
+#	
+#	def factory(dev):
+#		d = AMDevice(dev)
+#		d.connect()
+#		afc = AFCMediaDirectory(d)
+#		printdir(afc, u'/') # recursive print of all files visible
+#
+#		AMDSetLogLevel(0xff)
+#		afc.transfer_application(u'/Users/cooper/Documents/Dev/iOS/DeveloperDiskImg/Applications/MobileReplayer.app')
+#		AMDSetLogLevel(0x0)
+#
+#		printdir(afc, u'/') # recursive print of all files visible
+#
+#		afc.disconnect()
+#		return d
+#	
+#	handle_devices(factory)
 
