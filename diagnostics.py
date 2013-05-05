9321797c36bde844cadbe242f95448b9407da1c8
Mountainstorm <noreply@mountainstorm.co.uk>
2013-05-05 11:16:53 +0100

    Added documentation and command line access to most features

--- diagnostics.py
+++ diagnostics.py
@@ -28,6 +28,10 @@ from plistservice import *
 
 
 class Diagnostics(PlistService):
+	u'''Communicates with the diagnostic service and allows retrival of 
+	diagnostic information
+	'''
+
 	TYPE_ALL = u'All'
 	TYPE_WIFI = u'WiFi'
 	TYPE_GASGAUGE = u'GasGauge'
@@ -36,6 +40,9 @@ class Diagnostics(PlistService):
 	ACTION_WAIT_FOR_DISCONNECT = u'WaitForDisconnect'
 	ACTION_DISPLAY_PASS = u'DisplayPass'
 	ACTION_DISPLAY_FAIL = u'DisplayFail'
+
+	gestalt_names = [
+	]
 
 	def __init__(self, amdevice):
 		PlistService.__init__(
@@ -46,7 +53,6 @@ class Diagnostics(PlistService):
 		)
 
 	def disconnect(self):
-		self.goodbye()
 		PlistService.disconnect(self)
 
 	def _action(self, name, actions):
@@ -64,21 +70,26 @@ class Diagnostics(PlistService):
 			retval = True
 		return retval
 
-	def goodbye(self):
-		self._sendmsg({u'Request': u'Goodbye'})
-		return self._chkresult(self._recvmsg())
-
 	def sleep(self):
+		u'''Sends the device to sleep; disconnecting it from the host'''
 		self._sendmsg({u'Request': u'Sleep'})
 		return self._chkresult(self._recvmsg())
 
 	def restart(self, actions=[]):
+		u'''Restarts the device'''
 		return self._action(u'Restart', actions)
 
 	def shutdown(self, actions=[]):
+		u'''Shutsdownt the host'''
 		return self._action(u'Shutdown', actions)
 
 	def diagnostics(self, typ):
+		u'''Retrieves diagnostic information from the device; i.e. gasguage,
+		wifi, nand.
+
+		Arguments:
+		typ -- one of the ACTION_* defines
+		'''
 		retval = None
 		self._sendmsg({u'Request': typ})
 		msg = self._recvmsg()
@@ -88,17 +99,29 @@ class Diagnostics(PlistService):
 
 	# look in com.apple.mobilegestalt.plist for values
 	def mobilegestalt(self, keys):
+		u'''Retrieves mobile gestalt values
+
+		Arguments:
+		keys -- an array of keys to retrieve
+		'''
 		self._sendmsg({
 			u'Request': u'MobileGestalt',
 			u'MobileGestaltKeys': keys
 		})
 		msg = self._recvmsg()
 		if self._chkresult(msg):
-			retval = msg[u'Diagnostics']
+			retval = msg[u'Diagnostics'][u'MobileGestalt']
 		return retval
 
 	# IODeviceTree, IOPower, IOService
 	def ioregistry(self, plane=u'IOService', name=None, cls=None):
+		u'''Retrieves IORegistry keys and values
+
+		Arguments:
+		plane -- a IOReg plane name; IODeviceTree, IOPower, IOService
+		name -- the name of the key to retrieve
+		cls -- the class of the key to retrieve
+		'''
 		msg = {
 			u'Request': u'IORegistry',
 			u'CurrentPlane': plane
@@ -115,25 +138,146 @@ class Diagnostics(PlistService):
 		return retval
 
 
-if __name__ == u'__main__':
+def register_argparse_diag(cmdargs):
+	import argparse
 	import pprint
+	import sys
 
-	def factory(dev):
-		d = AMDevice(dev)
-		d.connect()
-		diag = Diagnostics(d)
-		print diag.diagnostics(Diagnostics.TYPE_GASGAUGE)
-		print diag.diagnostics(Diagnostics.TYPE_WIFI)
-		print diag.diagnostics(Diagnostics.TYPE_NAND)
-		print diag.diagnostics(Diagnostics.TYPE_ALL)
-		print
-		print diag.mobilegestalt([u'UserAssignedDeviceName'])
-		print
-		pprint.pprint(diag.ioregistry())
-		print
-		diag.shutdown()
+	def ioreg_dmp(d, pad=u''):
+		# XXX make pretty
+		pprint.pprint(d)
+
+	def cmd_gestalt(args, dev):
+		diag = Diagnostics(dev)
+		if args.name is not None:
+			print(diag.mobilegestalt([args.name.decode(u'utf-8')]))
+		else:
+			print(diag.mobilegestalt(Diagnostics.gestalt_names))
 		diag.disconnect()
-		return d
-	
-	handle_devices(factory)
+
+	def cmd_all(args, dev):
+		diag = Diagnostics(dev)
+		pprint.pprint(diag.diagnostics(Diagnostics.TYPE_ALL))
+		diag.disconnect()
+
+	def cmd_ioreg(args, dev):
+		diag = Diagnostics(dev)
+		plane = u'IOService'
+		name = None
+		cls = None
+		# XXX check the name/cls params actually work
+		if args.plane is not None:
+			plane = args.plane.decode(u'utf-8')
+		if args.name is not None:
+			name = args.name.decode(u'utf-8')
+		if args.cls is not None:
+			cls = args.cls.decode(u'utf-8')
+		ioreg_dmp(diag.ioregistry(plane, name, cls))
+		diag.disconnect()		
+
+	def cmd_sleep(args, dev):
+		diag = Diagnostics(dev)
+		diag.sleep()
+		diag.disconnect()
+
+	def cmd_restart(args, dev):
+		diag = Diagnostics(dev)
+		if args.quick:
+			diag.restart([Diagnostics.ACTION_DISPLAY_FAIL])
+		else:
+			diag.restart([Diagnostics.ACTION_DISPLAY_PASS])
+		diag.disconnect()
+
+	def cmd_shutdown(args, dev):
+		diag = Diagnostics(dev)
+		print args.quick
+		if args.quick:
+			diag.shutdown([Diagnostics.ACTION_DISPLAY_FAIL])
+		else:
+			diag.shutdown([Diagnostics.ACTION_DISPLAY_PASS])
+		diag.disconnect()
+
+	# diag command
+	diagparser = cmdargs.add_parser(
+		u'diag', 
+		help=u'retrieves diagnostic info; includes state changes like reboot, sleep etc'
+	)
+	diagcmd = diagparser.add_subparsers()
+
+	# mobile gestalt command
+	gestaltcmd = diagcmd.add_parser(
+		u'gestalt',
+		help=u'retrieves mobile gestalt values'
+	)
+	gestaltcmd.add_argument(
+		u'name',
+		nargs=u'?',
+		help=u'the name of the gestalt value to retrieve'
+	)
+	gestaltcmd.set_defaults(func=cmd_gestalt)
+
+	# all command
+	allcmd = diagcmd.add_parser(
+		u'all',
+		help=u'retrieves all diagnostic info; gasguage, wifi and nand'
+	)
+	allcmd.set_defaults(func=cmd_all)
+
+	# ioreg command
+	ioregcmd = diagcmd.add_parser(
+		u'ioreg',
+		help=u'retrieves ioregistry information from the device; defaults to the IOService plane'
+	)
+	ioregcmd.add_argument(
+		u'-p',
+		metavar=u'plane',
+		dest=u'plane',
+		help=u'the ioregistry plane to retrieve; IODeviceTree, IOPower, IOService etc'
+	)
+	ioregcmd.add_argument(
+		u'-n',
+		metavar=u'name',
+		dest=u'name',
+		help=u'ioregistry entry name to retrieve'
+	)
+	ioregcmd.add_argument(
+		u'-c',
+		metavar=u'class',
+		dest=u'cls',
+		help=u'ioregistry entry class to retrieve'
+	)
+	ioregcmd.set_defaults(func=cmd_ioreg)
+
+	# sleep command
+	sleepcmd = diagcmd.add_parser(
+		u'sleep',
+		help=u'put the device to sleep'
+	)
+	sleepcmd.set_defaults(func=cmd_sleep)
+
+	# restart command
+	restartcmd = diagcmd.add_parser(
+		u'restart',
+		help=u'restarts the device'
+	)
+	restartcmd.add_argument(
+		u'-q',
+		dest=u'quick',
+		action=u'store_true',
+		help=u'restarts quickly - without properly shutting down'
+	)	
+	restartcmd.set_defaults(func=cmd_restart)
+
+	# shutdown command
+	shutdowncmd = diagcmd.add_parser(
+		u'shutdown',
+		help=u'shutsdown the device'
+	)
+	shutdowncmd.add_argument(
+		u'-q',
+		dest=u'quick',
+		action=u'store_true',
+		help=u'shutsdown quickly'
+	)	
+	shutdowncmd.set_defaults(func=cmd_shutdown)
 
