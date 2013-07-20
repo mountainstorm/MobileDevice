8dd06722926b0792a50b29acd5ede63328f13cee
Cooper <none@ignore.com>
2013-07-20 14:29:41 +0100

    Added cmd to retrieve and delete crash logs from device

--- crashmover.py
+++ crashmover.py
@@ -60,16 +60,92 @@ class CrashMover(object):
 def register_argparse_crashmover(cmdargs):
 	import argparse
 	import sys
+	import afccrashlogdirectory
+	import posixpath
+	import stat
 
 	def cmd_crashmove(args, dev):
 		cm = CrashMover(dev)
 		cm.move_crashlogs()
 		cm.disconnect()
-		dev.disconnect()
+
+	def get_logs(afc, path, dest):
+		dirlist = []
+		for name in afc.listdir(path):
+			info = afc.lstat(posixpath.join(path, name))
+			if info.st_ifmt == stat.S_IFDIR:
+				dirlist.append((
+					posixpath.join(path, name),
+					os.path.join(dest, name)
+				))
+				try:
+					os.mkdir(os.path.join(dest, name))
+				except OSError:
+					pass # it already exists
+			else:
+				s = afc.open(posixpath.join(path, name), u'r')
+				d = open(os.path.join(dest, name), u'w+')
+				d.write(s.readall())
+				d.close()
+				s.close()
+		for names in dirlist:
+			get_logs(afc, names[0], names[1])
+
+	def del_logs(afc, path):
+		dirlist = []
+		for name in afc.listdir(path):
+			info = afc.lstat(posixpath.join(path, name))
+			if info.st_ifmt == stat.S_IFDIR:
+				dirlist.append(posixpath.join(path, name))
+			else:
+				afc.remove(posixpath.join(path, name))
+
+		for name in dirlist:
+			del_logs(afc, name)
+			afc.remove(name)
+
+	def cmd_crashget(args, dev):
+		# move the crashes
+		cm = CrashMover(dev)
+		cm.move_crashlogs()
+		cm.disconnect()
+
+		# retrieve the crashes
+		afc = afccrashlogdirectory.AFCCrashLogDirectory(dev)
+		get_logs(afc, u'/', args.dest.decode(u'utf-8'))
+
+		# optionally, delete the crashes
+		if args.delete_logs:
+			del_logs(afc, u'/')
+		afc.disconnect()
 
 	# cmd_crashmove command
-	crashmovecmd = cmdargs.add_parser(
-		u'crashmove', 
+	crashparser = cmdargs.add_parser(
+		u'crash', 
+		help=u'manipulates crash logs'
+	)
+
+	crashcmd = crashparser.add_subparsers()
+
+	crashmovecmd = crashcmd.add_parser(
+		u'move', 
 		help=u'moves crash logs into the afc directory'
 	)
 	crashmovecmd.set_defaults(func=cmd_crashmove)
+
+	# get the crash logs
+	crashgetcmd = crashcmd.add_parser(
+		u'get', 
+		help=u'retrieves crash logs from the device'
+	)
+	crashgetcmd.add_argument(
+		u'-d',
+		dest=u'delete_logs',
+		action=u'store_true',
+		help=u'if specified, delete the crash logs after retrieval'
+	)
+	crashgetcmd.add_argument(
+		u'dest',
+		help=u'destination directory; files are appended into it'
+	)	
+	crashgetcmd.set_defaults(func=cmd_crashget)
